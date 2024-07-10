0f2f7c49c5b46f456c17333aa0d75221fc4748a4
Yasin Oz <yasnoz@gmail.com>
2024-07-10 11:50:33 +0200

    Progress on reading cvs file: last name and phone numbers tests passing

--- phone_book.py
+++ phone_book.py
@@ -1,1 +1,14 @@
+'''first_name,last_name,phone_number
+John,Lennon,123
+George,Harrisson,456
+Ringo,Starr,789'''
 
+import csv
+
+with open('data/phone_book.csv', mode='r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    line_count = 0
+    next (csv_reader)
+    for row in csv_reader:
+          print(row[1:3])
+          line_count += 1
